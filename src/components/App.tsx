035ade9bd083f18a8106e8aa9adc61dec5a0e494
Stellakim1230 <stellakim1230@gmail.com>
2019-03-05 00:50:15 +0900

    pull request test commit.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 export interface HelloProps { compiler: string, framework: string}
 
 export const Hello = (props: HelloProps) =>
- <h1>Hello from {props.compiler} and {props.framework}!</h1>
+ <h1>HI from {props.compiler} and {props.framework}!</h1>
  
