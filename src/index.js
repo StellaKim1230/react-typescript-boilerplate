5e27d2d67604b2a8aef368eb6055201cff2a351b
Stellakim1230 <stellakim1230@gmail.com>
2019-03-26 21:08:03 +0900

    resolve conflict.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './components/App'
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 "use strict";
 exports.__esModule = true;
 var React = require("react");
 var ReactDOM = require("react-dom");
 var MainPage_1 = require("./pages/MainPage");
 ReactDOM.render(React.createElement(MainPage_1["default"], { title: '\uAC00\uACC4\uBD80' }), document.getElementById('root'));
->>>>>>> feature-init-typescript
